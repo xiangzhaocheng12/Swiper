210694e6ab80ea9c3fb5068a42ee9b4d919eae5c
xiangzhaocheng <420090428@qq.com>
2020-07-21 19:45:52 +0800

    完成了短信的收发

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from user import api
 
 urlpatterns = [
-    url(r'^vcode/fetch', api.sub_phnum,name='fetch'),
+    url(r'^vcode/fetch', api.fetch, name='fetch'),
+    url(r'^vcode/submit', api.submit, name='submit')
 ]
